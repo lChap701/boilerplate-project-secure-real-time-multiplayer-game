da2e0085210e9dc9fe837651c0b9cab8ffb9bd98
lChap701 <chapmanlucas201@gmail.com>
2021-10-26 14:15:45 -0500

    Added rendering

--- public/game.mjs
+++ public/game.mjs
@@ -2,6 +2,7 @@ import Player from "./Player.mjs";
 import Collectible from "./Collectible.mjs";
 import gameConfig from "./gameConfig.mjs";
 import loadSprite from "./utils/loadSprite.mjs";
+import drawUI from "./utils/drawUI.mjs";
 import shadows from "./utils/shadows.mjs";
 import startPos from "./utils/startPos.mjs";
 
@@ -11,6 +12,10 @@ const context = canvas.getContext("2d");
 
 const { playerSprites, collectibleSprites } = gameConfig;
 let player;
+let curCollectible;
+let playerRank = "Rank: 0 / 0";
+let opponents = [];
+let emitCollision = true;
 
 /* Sets the size of the canvas */
 canvas.width = gameConfig.gameWidth;
@@ -30,9 +35,112 @@ const itemSprites = Object.keys(collectibleSprites).map((sprite) =>
 
 /* Adds a new player to the game */
 socket.on("connect", () => {
-  const pos = startPos(gameConfig.size, playerSprites);
+  const pos = startPos(gameConfig.gameSize, playerSprites);
   player = new Player({ x: pos.x, y: pos.y, id: socket.id });
   socket.emit("joinGame", player);
 });
 
-socket.on("init", ({ players, collectible }) => {});
+/* Gets new opponents */
+socket.on("getOpponents", (players) => {
+  players.filter((p) => p.id !== player.id).forEach((opponent) => {
+    opponents.push(new Player(opponent));
+  });
+  socket.emit("requestRank");
+});
+
+/* Checks if collectible was taken and changes the current collectible */
+socket.on("setCollectible", (collectible) => {
+  if (curCollectible) {
+    player.score += curCollectible.value;
+    socket.emit("scored", player);
+    emitCollision = true;
+  }
+
+  curCollectible = new Collectible(collectible);
+});
+
+/* Gets the current rank of the player */
+socket.on("getRank", (players) => {
+  playerRank = player.calculateRank(players);
+});
+
+socket.on("opponentLeft", (id) => {
+  opponents = opponents.filter((opponent) => opponent.id !== id);
+  socket.emit("requestRank");
+});
+
+/* Allows the player to move */
+document.addEventListener("keydown", ({ key }) => {
+  switch (key) {
+    case "w":
+    case "ArrowUp":
+      player.dir = "up";
+      break;
+    case "s":
+    case "ArrowDown":
+      player.dir = "down";
+      break;
+    case "a":
+    case "ArrowLeft":
+      player.dir = "left";
+      break;
+    case "d":
+    case "ArrowRight":
+      player.dir = "right";
+  }
+
+  socket.emit("movePlayer", player);
+});
+
+/* Stops player from moving */
+document.addEventListener("keyup", ({ key }) => {
+  if (player && (key === "w" || key === "s" || key === "a" || key === "d")) {
+    player.dir = null;
+    socket.emit("movePlayer", player);
+  }
+});
+
+/**
+ * Draws sprites and creates the canavas for the ame
+ */
+function renderGame() {
+  canvas.style.background = "#080";
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draws game UI
+  drawUI(context, playerRank);
+
+  // Draws collectible
+  if (curCollectible) {
+    curCollectible.draw(context, itemSprites);
+  }
+
+  // Draws player's avatar
+  if (player) {
+    player.draw(context, playerAvatars[0]);
+  }
+
+  // Draws each opponent's avatar
+  for (const opponent of opponents) {
+    opponent.draw(context, playerAvatars[1]);
+  }
+
+  // Draws shadows on all sprites
+  shadows(context);
+
+  // Checks collision between player's avatar and collectible item
+  if (
+    player &&
+    curCollectible &&
+    player.collision(curCollectible) &&
+    emitCollision
+  ) {
+    socket.emit("playerCollideWithCollectible", player);
+    emitCollision = false;
+  }
+
+  requestAnimationFrame(renderGame);
+}
+
+// Calls renderGame() on load
+requestAnimationFrame(renderGame);
