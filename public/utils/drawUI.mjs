d28a076a591318b5cc987382e182c2f322178e55
lChap701 <chapmanlucas201@gmail.com>
2021-10-26 14:14:46 -0500

    Added module for drawing the UI for the game

---
+++ public/utils/drawUI.mjs
@@ -0,0 +1,45 @@
+import gameConfig from "../gameConfig.mjs";
+
+const { title, controls, gameWidth, padding, infoHeight, gameSize } =
+  gameConfig;
+
+/**
+ * Draws the UI for the game
+ * @module ./public/utils/drawUI
+ *
+ * @param ctx             Used for rendering
+ * @param playerRank      Represents the player's rank
+ *
+ */
+export default function drawUI(ctx, playerRank) {
+  // Info border
+  ctx.beginPath();
+  ctx.rect(padding, infoHeight, gameSize.width, gameSize.height);
+  ctx.strokeStyle = "#a8a8ac";
+  ctx.stroke();
+  ctx.closePath();
+
+  // Game info text y position
+  const infoTextPosY = infoHeight / 1.5;
+
+  // Game controls
+  ctx.fillStyle = "#ffffff";
+  ctx.font = `13px 'Press Start 2P'`;
+  ctx.textAlign = "start";
+  ctx.fillText("Controls: " + controls, padding, infoTextPosY);
+
+  // Game title
+  ctx.font = `15px 'Press Start 2P'`;
+  ctx.textAlign = "center";
+  ctx.fillText(title, gameWidth / 2, infoTextPosY);
+
+  // Player's rank
+  ctx.font = `13px 'Press Start 2P'`;
+  ctx.textAlign = "end";
+  ctx.fillText(playerRank, gameWidth - padding, infoTextPosY);
+
+  ctx.beginPath();
+  ctx.rect(padding, infoHeight, gameSize.width, gameSize.height);
+  ctx.stroke();
+  ctx.closePath();
+}
