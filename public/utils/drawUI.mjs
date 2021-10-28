c7a46090f181e60fcf61575e4f58bf3090bac2b4
lChap701 <chapmanlucas201@gmail.com>
2021-10-28 13:20:40 -0500

    Added score to UI

--- public/utils/drawUI.mjs
+++ public/utils/drawUI.mjs
@@ -8,34 +8,39 @@ const { title, controls, gameWidth, padding, infoHeight, gameSize } =
  * @module ./public/utils/drawUI
  * @see https://github.com/pinglu85/fcc-secure-real-time-multiplayer-game/blob/main/public/drawUI.mjs
  *
- * @param ctx           Used for rendering
- * @param playerRank    Represents the player's rank
+ * @param context   Used for rendering
+ * @param score     Represents the player's score
+ * @param rank      Represents the player's rank
  *
  */
-export default function drawUI(ctx, playerRank) {
+export default function drawUI(context, score, rank) {
   // Info border
-  ctx.beginPath();
-  ctx.rect(padding, infoHeight, gameSize.width, gameSize.height);
-  ctx.strokeStyle = "#020702";
-  ctx.stroke();
-  ctx.closePath();
+  context.beginPath();
+  context.rect(padding, infoHeight, gameSize.width, gameSize.height);
+  context.strokeStyle = "#020702";
+  context.stroke();
+  context.closePath();
 
-  // Game info text y position
-  const infoTextPosY = infoHeight / 1.5;
-
-  // Game controls
-  ctx.fillStyle = "#ffffff";
-  ctx.font = `12px 'Press Start 2P'`;
-  ctx.textAlign = "start";
-  ctx.fillText(controls, padding, infoTextPosY);
+  // Text color
+  context.fillStyle = "#ffffff";
 
   // Game title
-  ctx.font = `14px 'Press Start 2P'`;
-  ctx.textAlign = "center";
-  ctx.fillText(title, gameWidth / 2, infoTextPosY);
+  context.font = `14px 'Press Start 2P'`;
+  context.textAlign = "center";
+  context.fillText(title, gameWidth / 2, infoHeight / 2.5);
+
+  // Game controls
+  context.font = `12px 'Press Start 2P'`;
+  context.textAlign = "start";
+  context.fillText(controls, padding, infoHeight - padding);
+
+  // Player's score
+  context.font = `12px 'Press Start 2P'`;
+  context.textAlign = "center";
+  context.fillText(score, gameWidth / 2, infoHeight - padding);
 
   // Player's rank
-  ctx.font = `12px 'Press Start 2P'`;
-  ctx.textAlign = "end";
-  ctx.fillText(playerRank, gameWidth - padding, infoTextPosY);
+  context.font = `12px 'Press Start 2P'`;
+  context.textAlign = "end";
+  context.fillText(rank, gameWidth - padding, infoHeight - padding);
 }
